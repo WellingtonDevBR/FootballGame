9a482f37053a2df9ee7098ffc9b82c59bfdaf21a
Gamezzar <wellingtonsantos@gamezzar.com>
2022-10-31 17:15:39 -0300

    square to place teams components

---
+++ src/components/body/DefaultSquare.jsx
@@ -0,0 +1,12 @@
+import styles from './DefaultSquare.module.css'
+
+export function DefaultSquare(props) {
+    return (
+        <div className={styles.defaultSquare}>
+            <h4>
+                <p>Test</p>
+            </h4>
+        </div>
+
+    )
+}
---
+++ src/components/body/RoundOfSixteen.jsx
@@ -0,0 +1,16 @@
+import styles from './RoundOfSixteen.module.css';
+import React from 'react';
+
+export function FinalSixteen(props) {
+    return (
+        <div className={styles.roundOfSixteen}>
+            <h5>
+                <p>GRUPO</p>
+                <p>A</p>
+                <p>Time</p>
+            </h5>
+        </div>
+
+    )
+}
+
