419a79b685858fa2519e203611f907f569d2c7a6
Gamezzar <wellingtonsantos@gamezzar.com>
2022-10-31 20:34:47 -0300

    implemented semi finals phase

--- src/game/implementation/Phase.js
+++ src/game/implementation/Phase.js
@@ -94,3 +94,85 @@ export class RoundOfEightPhase extends IPhase {
     }, []);
   }
 }
+
+export class QuarterFinalsPhase extends IPhase {
+  constructor(matchType, classificationRule) {
+    super();
+    this.matchType = matchType;
+    this.classificationRule = classificationRule;
+    this.matchesHistory = [];
+  }
+
+  start(teams) {
+    const matches = this.matchType.match(teams);
+    this.addMatchesToHistory(matches);
+    return matches;
+  }
+
+  classify(teams) {
+    const classifiedTeams = this.classificationRule.classifyTeams(teams);
+    return this.prepareTeamsForMatch(classifiedTeams);
+  }
+
+  addMatchesToHistory(matches) {
+    this.matchesHistory.push(...matches);
+  }
+
+  getMatchesResults() {
+    return new PhaseResult(this.matchesHistory);
+  }
+
+  prepareTeamsForMatch(teams) {
+    return this.createGroups(teams, 2);
+  }
+
+  createGroups(teams, teamsPerGroup) {
+    return teams?.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / teamsPerGroup);
+      if (!resultArray[chunkIndex]) resultArray[chunkIndex] = [];
+      resultArray[chunkIndex].push(item);
+      return resultArray;
+    }, []);
+  }
+}
+
+export class SemiFinalsPhase extends IPhase {
+  constructor(matchType, classificationRule) {
+    super();
+    this.matchType = matchType;
+    this.classificationRule = classificationRule;
+    this.matchesHistory = [];
+  }
+
+  start(teams) {
+    const matches = this.matchType.match(teams);
+    this.addMatchesToHistory(matches);
+    return matches;
+  }
+
+  classify(teams) {
+    const classifiedTeams = this.classificationRule.classifyTeams(teams);
+    return this.prepareTeamsForMatch(classifiedTeams);
+  }
+
+  addMatchesToHistory(matches) {
+    this.matchesHistory.push(...matches);
+  }
+
+  getMatchesResults() {
+    return new PhaseResult(this.matchesHistory);
+  }
+
+  prepareTeamsForMatch(teams) {
+    return this.createGroups(teams, 2);
+  }
+
+  createGroups(teams, teamsPerGroup) {
+    return teams?.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / teamsPerGroup);
+      if (!resultArray[chunkIndex]) resultArray[chunkIndex] = [];
+      resultArray[chunkIndex].push(item);
+      return resultArray;
+    }, []);
+  }
+}
