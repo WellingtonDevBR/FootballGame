bb002ff330a328a94415185c1f3a4cc63cb2ad4b
Gamezzar <wellingtonsantos@gamezzar.com>
2022-10-31 18:42:38 -0300

    implementation of rules for score in the matches

--- src/game/implementation/Score.js
+++ src/game/implementation/Score.js
@@ -1,5 +1,29 @@
 export class Score {
-  constructor() {}
+  constructor() {
+    this.scoreForWinner = 3;
+    this.scoreForLoser = 0;
+    this.scoreForDraw = 1;
+    this.scoreA = 0;
+    this.scoreB = 0;
+  }
 
-  calculate(teamGoalsA, teamGoalsB) {}
+  calculate(teamGoalsA, teamGoalsB) {
+    if (teamGoalsA > teamGoalsB) {
+      this.scoreA = this.scoreForWinner;
+      this.scoreB = scoreForLoser;
+    } else if (teamGoalsA < teamGoalsB) {
+      this.scoreB = this.scoreForWinner;
+      this.scoreA = scoreForLoser;
+    } else {
+      this.scoreA = this.scoreForDraw;
+      this.scoreB = this.scoreForDraw;
+    }
+
+    return {
+      scoreA: this.scoreA,
+      scoreB: this.scoreB,
+      teamGoalsA,
+      teamGoalsB,
+    };
+  }
 }
