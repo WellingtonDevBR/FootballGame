ed3659cb1f79161290c846e72185ae1dd4aa29fa
Gamezzar <wellingtonsantos@gamezzar.com>
2022-10-31 19:09:32 -0300

    implement game class to start round of 16

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,7 +1,40 @@
+import { Round } from "./implementation/Round";
+import { Score } from "./implementation/Score";
+
 export class Game {
-  constructor() {}
+  constructor() {
+    this.teams = teams;
+    this.matchesHistory = [];
+    this.entryPhaseRoundsRule = 3;
+  }
 
-  start() {}
+  start() {
+    // Entry Phase Games
+    const roundOfSixteenPhase = new EntryPhase(
+      this.teams,
+      new EntryPhaseMatchType(
+        new Round(this.entryPhaseRoundsRule),
+        new Score()
+      ),
+      new EntryPhaseClassificationRule()
+    );
+    let roundOfSixteenTeams = roundOfSixteenPhase.prepareTeamsForMatches();
+    const toBeClassifiedForRoundOfEight =
+      roundOfSixteenPhase.start(roundOfSixteenTeams);
+    const classifiedsForRoundOfEight = roundOfSixteenPhase.classify(
+      toBeClassifiedForRoundOfEight
+    );
+    const entryPhaseMatchesResults = roundOfSixteenPhase.getMatchesResults();
+    this.updateMatchesHistories(entryPhaseMatchesResults);
 
-  updateMatchesHistories() {}
+    return {
+      roundOfSixteenTeams,
+      classifiedsForRoundOfEight,
+      matchesHistory: this.matchesHistory,
+    };
+  }
+
+  updateMatchesHistories() {
+    this.matchesHistory.push(...phaseResult.matches);
+  }
 }
