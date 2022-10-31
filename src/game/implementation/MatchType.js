ac40a96548490ffc12368a6a9388735c2e72cbbd
Gamezzar <wellingtonsantos@gamezzar.com>
2022-10-31 20:25:04 -0300

    implement NextPhaseMatchType for any other besides round of 8 phase

--- src/game/implementation/MatchType.js
+++ src/game/implementation/MatchType.js
@@ -57,3 +57,56 @@ export class EntryPhaseMatchType extends IMatchType {
     return this.teamsBattles;
   }
 }
+
+export class NextPhaseMatchType extends MatchType {
+  constructor(score) {
+    super();
+    this.score = score;
+    this.matchsHistory = [];
+    this.teamsBattles = [];
+    this.counter = 0;
+    this.teamScore = {};
+    this.matches = [];
+  }
+  match(groups) {
+    for (let teams of groups) {
+        for (let teamA of teams) {
+          let adversaries = teams.filter((team) => team.Name != teamA.Name);
+          for (let teamB of adversaries) {
+            let canTeamAMatchTeamB =
+              !this.matchsHistory.includes(teamA.Name + " x " + teamB.Name) &&
+              !this.matchsHistory.includes(teamB.Name + " x " + teamA.Name);
+            if (
+              canTeamAMatchTeamB &&
+              !this.matches.includes(teamA.Name) &&
+              !this.matches.includes(teamB.Name)
+            ) {
+              this.matchsHistory.push(teamA.Name + " x " + teamB.Name);
+              this.matches.push(teamA.Name);
+              this.matches.push(teamB.Name);
+
+              this.teamScore = this.score.calculate(
+                Math.floor(Math.random() * 10),
+                Math.floor(Math.random() * 10)
+              );
+              this.teamsBattles.push({
+                teamA: teamA.Name,
+                teamB: teamB.Name,
+                teamAToken: teamA.Token,
+                teamBToken: teamB.Token,
+                teamAScore: this.teamScore.scoreA,
+                teamBScore: this.teamScore.scoreB,
+                teamAGroup: teamA.Group,
+                teamBGroup: teamB.Group,
+                teamAGoals: this.teamScore.teamGoalsA,
+                teamBGoals: this.teamScore.teamGoalsB,
+              });
+            }
+            if (this.matches.length === 2) this.matches = [];
+          }
+        }
+      this.counter++;
+    }
+    return this.teamsBattles;
+  }
+}
